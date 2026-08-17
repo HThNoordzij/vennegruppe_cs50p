51af1ed629630f2579ed998f1354c6004a85147d
Hanna Noordzij <hanna.noordzij@auticon.com>
2026-08-17 13:01:04 +0200

    init

--- project.py
+++ project.py
@@ -1,4 +1,33 @@
+import sys
+import argparse
+
+## implement OOP
+## make it an CML program
+## input must contain csv with students
+## other inputs like previous groups? or min/max number of kids per group?
+## build project first on GitHub before submitting to CS50P
+## student (name, grade, current group and previous seen students)
+## group (id, students, max students, min students, boy/girl ratio)
+## implement some scoring mechanism based on previous seen students and boy/girl ratio
+## recursion of scoring x times, keep best scoring groups
+
+
+class Student:
+    ...
+
+
+class Group:
+    ...
+
 def main():
+    ...
+
+
+def new_groups(names):
+    ...
+
+
+def score_groups(old, new):
     ...
 
 
