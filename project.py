292df4ca500919081fd3b240a3d63f5998ce87e4
Hanna Noordzij <hanna.noordzij@auticon.com>
2026-08-17 14:42:48 +0200

    project initiated in VS Code and GitHub

--- project.py
+++ project.py
@@ -2,10 +2,8 @@ import sys
 import argparse
 
 ## implement OOP
-## make it an CML program
 ## input must contain csv with students
 ## other inputs like previous groups? or min/max number of kids per group?
-## build project first on GitHub before submitting to CS50P
 ## student (name, grade, current group and previous seen students)
 ## group (id, students, max students, min students, boy/girl ratio)
 ## implement some scoring mechanism based on previous seen students and boy/girl ratio
