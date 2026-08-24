aaee8c8662c1b89fbc1cf25c3ab53d8d49b19bcf
TK <66561039+95therese@users.noreply.github.com>
2026-08-24 13:40:02 +0200

    Change class attribute name
    Changed the name of the global class attribute in
    Student from "id" to "_next_id".

--- project.py
+++ project.py
@@ -23,11 +23,11 @@ class Student:
             A list to track the students that the student has seen.
     """
 
-    id = 1
+    _next_id = 1
 
     def __init__(self, name, gender=None, seen_students=None):
-        self.id = Student.id
-        Student.id += 1
+        self.id = Student._next_id
+        Student._next_id += 1
         self.name = name
         self.gender = gender
         self.seen_students = seen_students if seen_students is not None else []
